2ceb35f93f3b1c50eabf83faa63159601224b3ca
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2023-05-13 19:59:04 +0300

    new prices/images json and python manipulation

---
+++ config/pricesJsonSplit.py
@@ -0,0 +1,24 @@
+import json
+
+arr = []
+
+with open('productPricesImages.json', 'r') as f:
+  data = json.load(f)
+  for row in data:
+    my_dict = {'product_id': row['product_id'], 'product_image': row['product_image']}
+    arr.append(my_dict)
+
+with open('images.json', 'w') as json_file:
+  json.dump(arr, json_file, indent=2)
+
+arr = []
+
+with open('productPricesImages.json', 'r') as f:
+  data = json.load(f)
+  for row in data:
+    my_dict = {'id': row['product_id'], 'prices': [{'date': row['date1'],'price': row['product_price1']}, {'date': row['date2'],'price': row['product_price2']}, {'date': row['date3'],'price': row['product_price3']}, {'date': row['date4'],'price': row['product_price4']}, {'date': row['date5'],'price': row['product_price5']}]}
+    arr.append(my_dict)
+
+my_dict = {"fetch_date": 1669190231, "data": arr}
+with open('prices.json', 'w') as json_file:
+  json.dump(my_dict, json_file, indent=2)
