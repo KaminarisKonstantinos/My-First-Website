ff4be249754cf56adab0102ed9574a283af50843
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2023-05-18 00:14:27 +0300

    upload finish for today

--- src/libs/uploadFile.php
+++ src/libs/uploadFile.php
@@ -4,10 +4,18 @@ session_start();
 $uploaddir = '../../uploadFiles/';
 
 switch ($_GET['id']) {
-    case (1): $filename='categories.json';
+    case 1: 
+        $filename='productsAndCategories.json';
+        break;
+    case 2: 
+        $filename='prices.json';
+        break;
+    case 3: 
+        $filename='pois.geojson';
+        break;
 }
 
-$uploadfile = $uploaddir . basename($_FILES['file']['name']);
+$uploadfile = $uploaddir . $filename;//basename($_FILES['file']['name']);
 
 if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
     $_SESSION['error'.$_GET['id']] = "File is valid, and was successfully uploaded.\n";
